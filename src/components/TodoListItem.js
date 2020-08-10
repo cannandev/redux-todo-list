e23001c1891dd86fc1b305387d3a7e8dae852a56
Claire Annan <claire.annan@gsa.gov>
2020-08-10 19:40:24 -0400

    Dispatch doneOnClick and deleteOnClick properties. Add styles around done items.

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { markAsComplete } from '../actions/todoActions'
+import { markAsComplete, deleteTodo } from '../actions/todoActions'
 
 // TODO: does todo have to be an object?
 
-export const TodoListItemBase = ({ todo }) => {
-  const buttonStyles = `usa-button usa-button--accent-cool usa-button--small margin-left-2`
+export const TodoListItemBase = ({ todo, deleteOnClick, doneOnClick }) => {
+  const deleteButtonStyle = `usa-button usa-button--default usa-button--small margin-left-2`
+  const doneButtonStyle = `usa-button usa-button--accent-cool usa-button--small margin-left-2`
+
   return (
     (
       <li>
         {todo.text}
-        {todo.isCompleted && <span>Done!</span>}
+        {todo.isCompleted && <span role="img" aria-label="done">✔️</span>
+        }
         {todo.isCompleted
-          ? <button className={buttonStyles}>Delete</button>
-          : <button className={buttonStyles}>Mark as Completed</button>}
+          ? <button
+            className={deleteButtonStyle} onClick={_ => deleteOnClick(todo.text)}>Delete</button>
+          : <button
+            className={doneButtonStyle} onClick={_ => doneOnClick(todo.text)}>Mark as Completed</button>}
       </li>
     )
   )
 }
 
 const mapDispatchToProps = dispatch => ({
-  // doneOnClick: dispatch(markAsComplete(state))
+  doneOnClick: text => dispatch(markAsComplete(text)),
+  deleteOnClick: text => dispatch(deleteTodo(text)),
 })
 
 export const TodoListItem = connect(null, mapDispatchToProps)(TodoListItemBase)
